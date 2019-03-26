743643e077acef6cf71c5d46a5294841353e2d20
Edward Wenger <>
2019-03-25 22:03:05 -0700

    Import module with embedding

---
+++ config.py
@@ -0,0 +1,6 @@
+import emod
+
+c1 = emod.Cohort(10)
+
+c2 = emod.Cohort(20)
+c2_count = c2.drawFraction(0.5)
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
 #include <pybind11/embed.h>
+
 namespace py = pybind11;
 using namespace py::literals;
 
 int main() {
     py::scoped_interpreter guard{};
 
-    auto kwargs = py::dict("name"_a="World", "number"_a=42);
-    auto message = "Hello, {name}! The answer is {number}"_s.format(**kwargs);
-    py::print(message);
+    py::module calc = py::module::import("config");
+    py::object result = calc.attr("c2_count");
+    int c2_count = result.cast<int>();
+
+    std::cout << "c2 count: " << c2_count << std::endl;
 }
