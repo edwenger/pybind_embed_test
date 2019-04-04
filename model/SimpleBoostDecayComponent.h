d21e809a086e6db85c1a5c132167e71dd92c7c02
Edward Wenger <>
2019-04-03 19:31:45 -0700

    Export some bindings to Python module

--- model/SimpleBoostDecayComponent.h
+++ model/SimpleBoostDecayComponent.h
@@ -16,7 +16,7 @@ public:
     SimpleBoostDecayComponent();
 
     void Configure(const ParamSet& ps) override;
-    void Update(float dt) override;
+    void Update(float dt=1.0f) override;
     void Notify(const std::string& event) override;
     float GetCurrentValue() const override;
 
---
+++ modules.cpp
@@ -0,0 +1,37 @@
+#include <string>
+
+#include <pybind11/pybind11.h>
+
+#include "core/ModelComponentFactory.h"
+#include "model/SimpleBoostDecayComponent.h"
+
+namespace py = pybind11;
+using namespace py::literals;
+
+PYBIND11_MODULE(dtk3, m) {
+
+    m.def("ModelComponent", &ModelComponentFactory::CreateComponent);
+
+    py::class_<SimpleBoostDecayComponent>(m, "SimpleBoostDecayComponent")
+            .def(py::init<>())
+            .def("Update",
+                 &SimpleBoostDecayComponent::Update,
+                 "Update (i.e. Decay at decayRate) currentValue for dt",
+                 "dt"_a=1
+             )
+            .def("Notify",
+                 &SimpleBoostDecayComponent::Notify,
+                 "Notify of an event (e.g. 'Boost')",
+                 "event"_a="Boost"
+             )
+            .def_property_readonly(
+                "currentValue",
+                &SimpleBoostDecayComponent::GetCurrentValue
+            )
+            .def("__repr__",
+                [](const SimpleBoostDecayComponent &c) {
+                    std::string s = std::to_string(c.GetCurrentValue());
+                    return "<dtk3.SimpleBoostDecayComponent with currentValue of " + s + ">";
+                }
+            );
+}
